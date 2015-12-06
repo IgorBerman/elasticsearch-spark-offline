d389e6ea8e5cf3f7336145965f84f70883630924
Igor Berman <igor@dynamicyield.com>
2015-12-06 12:24:15 +0200

    using es hadoop jsonizer to be compatible

--- src/main/java/com/dy/spark/elasticsearch/ESIndexShardSnapshotCreator.java
+++ src/main/java/com/dy/spark/elasticsearch/ESIndexShardSnapshotCreator.java
@@ -30,12 +30,13 @@ import org.elasticsearch.common.base.Joiner;
 import org.elasticsearch.common.settings.ImmutableSettings;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.hadoop.serialization.builder.ContentBuilder;
+import org.elasticsearch.hadoop.util.FastByteArrayOutputStream;
 import org.elasticsearch.node.Node;
 import org.elasticsearch.plugins.PluginsService;
+import org.elasticsearch.spark.serialization.ScalaValueWriter;
 
 import scala.Tuple2;
-
-import com.google.gson.Gson;
 
 @Log4j
 @RequiredArgsConstructor
@@ -51,37 +52,45 @@ public class ESIndexShardSnapshotCreator implements Serializable {
 	private final String templateName;
 	private final String templateJson;
 	private final int maxMergedSegment;
-	private final int flushSizeInMb; 
+	private final int flushSizeInMb;
 
-	public <T> void create(FileSystem fs, String indexName, int partition, int bulkSize, String routing, String indexType, Iterator<Tuple2<String, T>> docs, long timeout) throws IOException {
+	public <T> void create(
+			FileSystem fs, 
+			String indexName, 
+			int partition, 
+			int bulkSize, 
+			String routing,
+			String indexType, 
+			Iterator<Tuple2<String, T>> docs, 
+			long timeout) throws IOException {
 		createSnapshotAndMoveToDest(fs, indexName, partition, 1, bulkSize, routing, indexType, docs, timeout, true);
 	}
-	
-	private <T> void createSnapshotAndMoveToDest(FileSystem fs, 
+
+	private <T> void createSnapshotAndMoveToDest(
+			FileSystem fs, 
 			String indexName, 
 			int partition, 
 			int numShardsPerIndex,
-			int bulkSize,
+			int bulkSize, 
 			String routing, 
 			String indexType, 
 			Iterator<Tuple2<String, T>> docs, 
-			long timeout, 
+			long timeout,
 			boolean moveShards) throws IOException {
-		log.info("Creating snapshot of shard for index " + indexName + "[" + partition +"]");
-		String snapshotWorkingLocation = Joiner.on("/").join(snapshotWorkingLocationBase, snapshotRepoName, indexName, partition);
+		log.info("Creating snapshot of shard for index " + indexName + "[" + partition + "]");
+		String snapshotWorkingLocation = Joiner.on("/").join(snapshotWorkingLocationBase, snapshotRepoName, indexName,
+				partition);
 		log.debug("snapshotWorkingLocation " + snapshotWorkingLocation);
 		String esWorkingDir = Joiner.on("/").join(esWorkingBaseDir, indexName, partition);
 		log.debug("esWorkingDir " + esWorkingDir);
 		String nodeName = Joiner.on("_").join("embededESTempLoaderNode", indexName, partition);
 		String clusterName = Joiner.on("_").join("embededESTempLoaderCluster", indexName, partition);
-		log.debug("node/cluster " + nodeName + "/"+clusterName);
-		
-		
+		log.debug("node/cluster " + nodeName + "/" + clusterName);
+
 		org.elasticsearch.common.settings.ImmutableSettings.Builder builder = ImmutableSettings
 				.builder()
 				// Disable HTTP transport, we'll communicate inner-jvm
-				.put("http.enabled", false)
-				.put("processors", 1)
+				.put("http.enabled", false).put("processors", 1)
 				.put(IndexMetaData.SETTING_NUMBER_OF_SHARDS, numShardsPerIndex)
 				.put("node.name", nodeName)
 				.put("path.home", esWorkingDir)
@@ -122,57 +131,74 @@ public class ESIndexShardSnapshotCreator implements Serializable {
 			settings.put("location", snapshotWorkingLocation);
 			settings.put("compress", true);
 			settings.put("max_snapshot_bytes_per_sec", "1024mb"); // The default
-																	// 20mb/sec is
-																	// very slow for
-																	// a local disk
+																	// 20mb/sec
+																	// is
+																	// very slow
+																	// for
+																	// a local
+																	// disk
 																	// to disk
 																	// snapshot
-			node.client().admin().cluster().preparePutRepository(snapshotRepoName).setType("fs").setSettings(settings).get();
-			
-			log.debug("Creating index " + indexName + "[" + partition +"]" + " with 0 replicas and " + numShardsPerIndex + " number of shards");
-			node.client().admin().indices().prepareCreate(indexName)
-					.setSettings(settingsBuilder()
-							.put("index.number_of_replicas", 0)
-							.put("index.number_of_shards", numShardsPerIndex)
-							//.put("index.merge.policy.max_merge_at_once", 10)
-							//.put("index.merge.policy.segments_per_tier", 15)//should be more than max_merge_at_once
-							.put("index.refresh_interval", -1)//this one is VERY important...
-							// The default 5gb segment max size is too large for the typical
-							// hadoop node
-							.put("index.merge.policy.max_merged_segment", maxMergedSegment)
-							.put("index.merge.scheduler.max_thread_count", 1)
-							.put("index.load_fixed_bitset_filters_eagerly", false)
-							.put("index.compound_format", false) 
-							//Aggressive flushing helps keep the memory footprint
-							.put("index.translog.flush_threshold_size", flushSizeInMb+"mb")
-							).get();
+			node.client().admin().cluster().preparePutRepository(snapshotRepoName).setType("fs").setSettings(settings)
+					.get();
 
-			log.info("Starting indexing documents "+ indexName + "[" + partition +"]");
+			log.debug("Creating index " + indexName + "[" + partition + "]" + " with 0 replicas and "
+					+ numShardsPerIndex + " number of shards");
+			node.client()
+					.admin()
+					.indices()
+					.prepareCreate(indexName)
+					.setSettings(
+							settingsBuilder()
+									.put("index.number_of_replicas", 0)
+									.put("index.number_of_shards", numShardsPerIndex)
+									// .put("index.merge.policy.max_merge_at_once",
+									// 10)
+									// .put("index.merge.policy.segments_per_tier",
+									// 15)//should be more than
+									// max_merge_at_once
+									.put("index.refresh_interval", -1)
+									// this one is VERY important...
+									// The default 5gb segment max size is too
+									// large for the typical
+									// hadoop node
+									.put("index.merge.policy.max_merged_segment", maxMergedSegment)
+									.put("index.merge.scheduler.max_thread_count", 1)
+									.put("index.load_fixed_bitset_filters_eagerly", false)
+									.put("index.compound_format", false)
+									// Aggressive flushing helps keep the memory
+									// footprint
+									.put("index.translog.flush_threshold_size", flushSizeInMb + "mb")).get();
+
+			log.info("Starting indexing documents " + indexName + "[" + partition + "]");
 			BulkRequestBuilder bulkRequest = node.client().prepareBulk();
-			Gson gson = new Gson();
-			
+
+			ScalaValueWriter scalaValueWriter = new ScalaValueWriter();
 			int total = 0;
 			int countInBulk = 0;
 			while (docs.hasNext()) {
 				Tuple2<String, T> doc = docs.next();
 
-				IndexRequestBuilder indexRequestBuilder = node
-						.client()
-						.prepareIndex(indexName, indexType)
-						.setId(doc._1())
-				// 		.setRouting(routing)
-						.setSource(gson.toJson(doc._2()));
-				bulkRequest.add(indexRequestBuilder);
-				countInBulk ++;
-				total++;
+				FastByteArrayOutputStream bos = new FastByteArrayOutputStream();
+		        ContentBuilder.generate(bos, scalaValueWriter).value(doc._2()).flush().close();		        
 				
+				byte[] docBytes = bos.bytes().bytes();
+				
+				IndexRequestBuilder indexRequestBuilder = node.client().prepareIndex(indexName, indexType)
+						.setId(doc._1())
+						// .setRouting(routing)
+						.setSource(docBytes);
+				bulkRequest.add(indexRequestBuilder);
+				countInBulk++;
+				total++;
+
 				if (countInBulk == bulkSize) {
 					submitBulk(bulkRequest, total);
 					countInBulk = 0;
 					bulkRequest = node.client().prepareBulk();
 				}
 				if (total % 100000 == 0) {
-					log.info("Cont indexing " + indexName + "[" + partition +"], processed " + total);
+					log.info("Cont indexing " + indexName + "[" + partition + "], processed " + total);
 				}
 			}
 			if (countInBulk != 0) {
@@ -181,28 +207,31 @@ public class ESIndexShardSnapshotCreator implements Serializable {
 
 			TimeValue v = new TimeValue(timeout);
 
-			log.info("Flushing "+indexName+ "[" + partition +"]");
+			log.info("Flushing " + indexName + "[" + partition + "]");
 			node.client().admin().indices().prepareFlush(indexName).get(v);
 
-			log.info("Optimizing "+indexName+ "[" + partition +"]");
+			log.info("Optimizing " + indexName + "[" + partition + "]");
 			node.client().admin().indices().prepareOptimize(indexName).get(v);
 
 			String snapshotName = Joiner.on("_").join(SNAPSHOT_NAME_PREFIX, indexName);
-			log.info("Snapshoting "+indexName + "[" + partition +"]" + " as " + snapshotName + " to snapshot repo " + snapshotRepoName);
+			log.info("Snapshoting " + indexName + "[" + partition + "]" + " as " + snapshotName + " to snapshot repo "
+					+ snapshotRepoName);
 			node.client().admin().cluster().prepareCreateSnapshot(snapshotRepoName, snapshotName)
 					.setWaitForCompletion(true).setIndices(indexName).get();
 
-			log.info("Deleting " + indexName+ "[" + partition +"]");
+			log.info("Deleting " + indexName + "[" + partition + "]");
 			ActionFuture<DeleteIndexResponse> response = node.client().admin().indices()
 					.delete(new DeleteIndexRequest(indexName));
 			while (!response.isDone()) {
 				waitForCompletion();
 			}
 
-			log.info("Moving shard snapshot of " + indexName+ "[" + partition +"]" + " to destination " + snapshotDestination);
-			transport.move(fs, snapshotName, indexName, snapshotWorkingLocation, snapshotDestination, partition, moveShards);
+			log.info("Moving shard snapshot of " + indexName + "[" + partition + "]" + " to destination "
+					+ snapshotDestination);
+			transport.move(fs, snapshotName, indexName, snapshotWorkingLocation, snapshotDestination, partition,
+					moveShards);
 
-			log.info("Deleting snapshot of " + indexName+ "[" + partition +"]" + snapshotName);
+			log.info("Deleting snapshot of " + indexName + "[" + partition + "]" + snapshotName);
 			node.client().admin().cluster().prepareDeleteSnapshot(snapshotRepoName, snapshotName).execute().actionGet();
 
 			node.close();
@@ -242,9 +271,16 @@ public class ESIndexShardSnapshotCreator implements Serializable {
 	/**
 	 * We create another index snapshot for number of shards
 	 */
-	public void postprocess(FileSystem fs, String indexName, int numShardsPerIndex, String routing, String indexType, long timeout) throws IOException {
+	public void postprocess(
+			FileSystem fs, 
+			String indexName, 
+			int numShardsPerIndex, 
+			String routing, 
+			String indexType,
+			long timeout) throws IOException {
 		Iterator<Tuple2<String, Object>> docs = new ArrayList<Tuple2<String, Object>>().iterator();
-		createSnapshotAndMoveToDest(fs, indexName, numShardsPerIndex, numShardsPerIndex, 0, routing, indexType, docs, timeout, false);
+		createSnapshotAndMoveToDest(fs, indexName, numShardsPerIndex, numShardsPerIndex, 0, routing, indexType, docs,
+				timeout, false);
 	}
-	
+
 }
--- src/test/java/com/dy/spark/elasticsearch/ESIndexShardSnapshotCreatorTest.java
+++ src/test/java/com/dy/spark/elasticsearch/ESIndexShardSnapshotCreatorTest.java
@@ -1,7 +1,10 @@
 package com.dy.spark.elasticsearch;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.Serializable;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -26,36 +29,37 @@ import com.google.common.io.Files;
 
 public class ESIndexShardSnapshotCreatorTest implements Serializable {
 	private static final int TIMEOUT = 10000;
-		protected transient JavaSparkContext sc;
-		protected File tempDir;
+	protected transient JavaSparkContext sc;
+	protected File tempDir;
 
-		@Before
-		public void setUp() throws Exception {
-			tempDir = Files.createTempDir();
-			tempDir.deleteOnExit();
-			SparkConf sparkConf = new SparkConf();
-			sparkConf.setMaster("local[*]");
-			sparkConf.setAppName(getClass().getSimpleName());
-			sparkConf.set("spark.local.dir", tempDir+"/spark");
-			sc = new JavaSparkContext(sparkConf);
-			sc.setCheckpointDir(tempDir+"/checkpoint/");
+	@Before
+	public void setUp() throws Exception {
+		tempDir = Files.createTempDir();
+		tempDir.deleteOnExit();
+		SparkConf sparkConf = new SparkConf();
+		sparkConf.setMaster("local[*]");
+		sparkConf.setAppName(getClass().getSimpleName());
+		sparkConf.set("spark.local.dir", tempDir + "/spark");
+		sc = new JavaSparkContext(sparkConf);
+		sc.setCheckpointDir(tempDir + "/checkpoint/");
+	}
+
+	@After
+	public void tearDown() {
+		if (sc != null) {
+			sc.stop();
+			sc = null;
 		}
-		@After
-		public void tearDown() {
-			if (sc != null) {
-				sc.stop();
-				sc = null;
-			}
-			FileUtils.deleteQuietly(tempDir);
-		}
-	
+		FileUtils.deleteQuietly(tempDir);
+	}
+
 	@RequiredArgsConstructor
 	@Data
-	static final class MyData implements Serializable {
-		public final int a;
-		public final String b;
+	public static final class MyData implements Serializable {
+		final int a;
+		final String b;
 	}
-	
+
 	static final class ConfigSupplier implements Supplier<Configuration>, Serializable {
 		@Override
 		public Configuration get() {
@@ -64,54 +68,75 @@ public class ESIndexShardSnapshotCreatorTest implements Serializable {
 	}
 
 	@Test
-	public void test() throws Exception {		
+	public void test() throws Exception {
 		String snapshotBase = tempDir + "/es-test/snapshots-work/";
-		String esWorkingBaseDir=tempDir + "/es-test/es-work/";
-		File templateFile = new File(ESIndexShardSnapshotCreatorTest.class.getResource("template.json").toURI().toURL().getFile());
-		String templateJson = FileUtils.readFileToString(templateFile);
-		
+		String esWorkingBaseDir = tempDir + "/es-test/es-work/";
 		String destination = "file://" + tempDir + "/es-test/my_backup_repo/";
-		
+
+		createSnapshot(snapshotBase, esWorkingBaseDir, destination);
+	}
+
+	private void createSnapshot(String snapshotBase, String esWorkingBaseDir, String destination)
+			throws MalformedURLException, URISyntaxException, IOException, Exception {
+		File templateFile = new File(ESIndexShardSnapshotCreatorTest.class.getResource("template.json").toURI().toURL()
+				.getFile());
+		String templateJson = FileUtils.readFileToString(templateFile);
+
 		ESFilesTransport transport = new ESFilesTransport();
-		final ESIndexShardSnapshotCreator creator = new ESIndexShardSnapshotCreator(transport, 
-				snapshotBase, 
-				destination,
-				"my_backup_repo", 
-				esWorkingBaseDir, 
-				"es-template", 
-				templateJson,
-				100,
-				512);
-		final String indexName = "my-index_" +System.currentTimeMillis();
-		final String indexType = "mydata";//should be consistent with template.json
+		String snapshotRepoName = "my_backup_repo";
+		final ESIndexShardSnapshotCreator creator = new ESIndexShardSnapshotCreator(transport, snapshotBase,
+				destination, snapshotRepoName, esWorkingBaseDir, "es-template", templateJson, 100, 512);
+		final String indexName = "my-index_" + System.currentTimeMillis();
+		final String indexType = "mydata";// should be consistent with
+											// template.json
 		Supplier<Configuration> configurationSupplier = new ConfigSupplier();
 		final int bulkSize = 10000;
-		ESIndexShardSnapshotPipeline<MyData> pipeline = new ESIndexShardSnapshotPipeline<>(
-				creator, 
-				configurationSupplier, 
-				indexName,
-				indexType, 
-				bulkSize, 
-				TIMEOUT);
-		
+		ESIndexShardSnapshotPipeline<MyData> pipeline = new ESIndexShardSnapshotPipeline<>(creator,
+				configurationSupplier, indexName, indexType, bulkSize, TIMEOUT);
+
 		long start = System.currentTimeMillis();
 		final int partitionsNum = 4;
-		final int totalNumberOfDocsPerPartition = 10;//5_000_000;//1_078_671_786;
-		
+		final int totalNumberOfDocsPerPartition = 10;// 5_000_000;//1_078_671_786;
+
 		List<Tuple2<String, MyData>> docs = new ArrayList<>();
 		for (int part = 0; part < partitionsNum; part++) {
 			final int partNum = part;
-			for (int doc = 0; doc <  totalNumberOfDocsPerPartition; doc++) {
-				String id = doc+"-" +partNum;
+			for (int doc = 0; doc < totalNumberOfDocsPerPartition; doc++) {
+				String id = doc + "-" + partNum;
 				docs.add(new Tuple2<>(id, new MyData(doc, id)));
 			}
 		}
-		JavaPairRDD<String,MyData> pairRDD = sc.parallelizePairs(docs).partitionBy(new HashPartitioner(partitionsNum));		
-		
+		JavaPairRDD<String, MyData> pairRDD = sc.parallelizePairs(docs).partitionBy(new HashPartitioner(partitionsNum));
+
 		pipeline.process(pairRDD);
-		
-		
-		System.out.println("Everything took: " + TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis() - start) + " secs");
+
+		System.out.println("Everything took: " + TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis() - start)
+				+ " secs");
+		System.out.println("Restore with :\n curl -XPOST '"
+				+ String.format("http://localhost:9200/_snapshot/%s/snapshot_%s/_restore", snapshotRepoName, indexName)
+				+ "'");
+	}
+
+	/*
+	 * 0. create /tmp/es-test/my_backup_repo dir and give it all permissions
+	 * 1. create on local elasticsearch snapshot repo
+	 curl -XPUT 'http://localhost:9200/_snapshot/my_backup_repo' -d '{
+	 "type": "fs",
+		  "settings": {
+		  "location": "/tmp/es-test/my_backup_repo/",
+		  "compress": true
+	 	}
+	 }'
+	 */
+	public static void main(String[] args) throws Exception {
+		String snapshotBase = "/tmp/es-test/snapshots-work/";
+		String esWorkingBaseDir = "/tmp/es-test/es-work/";
+		String snapshotFinalDestination = "file:///tmp/es-test/my_backup_repo/";
+
+		ESIndexShardSnapshotCreatorTest test = new ESIndexShardSnapshotCreatorTest();
+		test.setUp();
+		test.createSnapshot(snapshotBase, esWorkingBaseDir, snapshotFinalDestination);
+
 	}
 
 }
